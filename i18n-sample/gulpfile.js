61e197a55d50064f96c30f18442b65b6fd97fc27
William Xiang <william.xiang@ibm.com>
2022-07-08 14:35:43 -0400

    change the sample to extract the localized strings

--- i18n-sample/gulpfile.js
+++ i18n-sample/gulpfile.js
@@ -48,7 +48,9 @@ const doCompile = function (buildNls) {
 		.pipe(sourcemaps.init())
 		.pipe(tsProject()).js
 		.pipe(buildNls ? nls.rewriteLocalizeCalls() : es.through())
-		.pipe(buildNls ? nls.createAdditionalLanguageFiles(languages, 'i18n', 'out') : es.through());
+		.pipe(buildNls ? nls.createAdditionalLanguageFiles(languages, 'i18n', 'out') : es.through())
+		.pipe(buildNls ? nls.bundleMetaDataFiles('i18n-sample', 'out') : es.through())
+		.pipe(buildNls ? nls.bundleLanguageFiles() : es.through())
 
 	if (inlineMap && inlineSource) {
 		r = r.pipe(sourcemaps.write());
@@ -72,11 +74,31 @@ const vscePackageTask = function() {
 	return vsce.createVSIX();
 };
 
+// export the strings to be translated from these files
+gulp.task('export-i18n', function() {
+	return gulp.src(['package.nls.json', 'out/nls.metadata.header.json', 'out/nls.metadata.json'])
+		.pipe(nls.createXlfFiles("vscode-extensions", "i18n-sample"))
+		.pipe(gulp.dest(path.join('vscode-translations-export')));
+})
+
+// After translating the strings in the exported xlf files, 
+gulp.task('import-i18n', () => {
+	return es.merge(languages.map(language => {
+		// console.log(language.folderName)
+		return gulp.src(["/Users/william/vscode/vscode-extension-samples-forked/i18n-sample/vscode-translations-export/vscode-extensions/i18n-sample.xlf"])
+			.pipe(nls.prepareJsonFiles())
+			.pipe(gulp.dest(path.join('./i18n', language.folderName)));
+	}));
+});
+
+
 gulp.task('default', buildTask);
 
 gulp.task('clean', cleanTask);
 
 gulp.task('compile', gulp.series(cleanTask, internalCompileTask));
+
+gulp.task('compileNLS', gulp.series(cleanTask, internalNlsCompileTask));
 
 gulp.task('build', buildTask);
 
